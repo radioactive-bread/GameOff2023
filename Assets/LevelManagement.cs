d4309c0137ef75c11c31ab8d61d87efffdfb27b0
radioactive-bread <150344613+radioactive-bread@users.noreply.github.com>
2023-11-29 22:05:38 +0100

    Summary 29.11

---
+++ Assets/LevelManagement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelManagement : MonoBehaviour
+{
+    public int YourConfessionsNumber;
+
+    public TextMeshProUGUI YourConfessionsOutput;
+    public TextMeshProUGUI TextRound;
+    // Start is called before the first frame update
+    void Start() {
+        YourConfessionsNumber = 0;
+        TheScenesManager.round++;
+        //int roundCopy = TheScenesManager.round;
+        Debug.Log(TheScenesManager.round);
+        TextRound.text = "Round " + TheScenesManager.round.ToString();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    
+    public void PickConfessionsPlus() {
+        if (YourConfessionsNumber < 7) {
+            YourConfessionsNumber++;
+            YourConfessionsOutput.text = YourConfessionsNumber.ToString();
+        }
+    }
+
+    public void PickConfessionsMinus() {
+        if (YourConfessionsNumber > 0) {
+            YourConfessionsNumber = YourConfessionsNumber - 1;
+            YourConfessionsOutput.text = YourConfessionsNumber.ToString();
+        }
+    }
+
+    /*private void GoToWeighingScene() {
+        TheScenesManager.Instance.LoadWeighing();
+    }*/
+
+}
---
+++ Assets/TheScenesManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheScenesManager : MonoBehaviour
+{
+    // Tutorial for future reference: https://youtu.be/jrPTpD2eAMw?feature=shared
+    /*public static TheScenesManager Instance;
+    // Start is called before the first frame update
+    private void Awake () {
+        Instance = this;
+    }
+
+     public enum Scene {
+        Menu,
+        Tutorial,
+        LevelScene,
+        WeighingScene,
+        FinalJudgement
+    }
+
+    public void LoadScene(Scene scene) {
+        TheScenesManager.LoadScene(scene.ToString());
+    }
+
+    public void LoadNewRound() {
+        TheScenesManager.LoadScene(Scene.LevelScene.ToString());
+    }
+
+    public void LoadWeighing() {
+        //if round < 6
+        TheScenesManager.LoadScene(Scene.WeighingScene.ToString());
+    }*/
+
+    public static int round; 
+
+    public void LoadWeighingScene() {
+        if (round < 6) {SceneManager.LoadScene("WeighingScene");}
+        else {SceneManager.LoadScene("FinalJudgment");}
+    }
+    
+    public void LoadMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadLevelScene() {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+
+}
